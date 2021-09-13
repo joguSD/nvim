e0e1a4c1ba24006cf09df662fbea393ec5da60d0
Jordan Guymon <jogu.sd@gmail.com>
2021-09-12 17:51:51 -0700

    Load plugins first
    This ensures errors later in cofigurations, such as colorscheme,
    don't stop the plugin boostrap from loading and allowing the required
    plugins to be installed.

--- init.lua
+++ init.lua
@@ -3,6 +3,7 @@ local home = os.getenv('HOME')
 vim.g.python3_host_prog = home .. '/.venvs/nvim3/bin/python'
 vim.g.python_host_prog = home .. '/.venvs/nvim2/bin/python'
 
-require('core')
+-- Load plugins first to ensure later config can rely on it
 require('plugins')
+require('core')
 require('config-plugins')
